96ed3b87735249419ed08dcd102c71b6e2ed5602
GABRIEL SUHETT <gabriel.suhett@external.stellantis.com>
2022-11-03 15:27:07 -0300

    Ajustes novo banco de dados

---
+++ DeliveryNat/Migrations/20221103151038_NovoBanco.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryNat.Migrations
+{
+    public partial class NovoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
